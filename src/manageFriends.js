bcb715f0fce9a0814cebcc00ed6e2543de54c74b
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-10-18 19:30:17 -0700

    commit tree 4304b857e0a781c7a785e36eaeb1a885173c1ae8

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,19 @@
-export function manageFriends(state, action) {
+// let state = { friends: []}
+
+export function manageFriends(state = { friends: [] }, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends: [...state.friends, action.payload]
+      }
+      case "friends/remove":
+        return { 
+          friends: state.friends.filter(friend => friend.id !== action.payload)
+        }
+
+    default: 
+      return state
+  }
+
 }
